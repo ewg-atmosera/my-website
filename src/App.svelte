36e940b18f9d42de1b6654a02cfe23784436bba7
Eric Greene <eric@t4d.io>
2024-12-10 09:09:04 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  type Color = {
+    id: number;
+    name: string;
+    hexcode: string;
+  }
+
+  let colors: Color[] = $state([])
+
+  // lifecycle method to call when the component is mounted
+  onMount(() => {
+    console.log('App component mounted')
+
+    fetch(`${import.meta.env.VITE_API_URL}/colors`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer a1sd422s5f3s212gfc3s',
+        'Organization': '1D5GHT',
+      },
+    })
+
+      .then(response => response.json())
+      .then(colorsData => colors = colorsData)
+  })
+  
+</script>
+
+<main>
+  <h1>Color Tool</h1>
+
+  <ul>
+    {#each colors as color}
+      <li>{color.name} - {color.hexcode}</li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+</style>
